f77ee0676922b6743e36bca93680b633a34b1a8a
mlhartme <mlhartme@a73564bc-5afb-411d-915a-889d56b9fb7d>
2012-01-05 06:39:35 +0000

    test for firsts()

--- src/test/java/net/sf/beezle/mork/grammar/GrammarTest.java
+++ src/test/java/net/sf/beezle/mork/grammar/GrammarTest.java
@@ -1,6 +1,10 @@
 package net.sf.beezle.mork.grammar;
 
+import net.sf.beezle.mork.misc.StringArrayList;
+import net.sf.beezle.sushi.util.IntBitSet;
 import org.junit.Test;
+
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,5 +16,28 @@ public class GrammarTest {
         g = Grammar.forProductions("S A B", "A a b", "B x y");
         assertEquals(3, g.getProductionCount());
         assertEquals(1, g.getAlternativeCount(g.getSymbolTable().indexOf("S")));
+    }
+
+    @Test
+    public void prefix() {
+        Grammar g;
+        IntBitSet nullable;
+        Map<Integer, PrefixSet> firsts;
+        StringArrayList symbolTable;
+
+        g = Grammar.forProductions("Z S",
+                "S S b",
+                "S b A a",
+                "A a S c",
+                "A a",
+                "A a S b");
+        symbolTable = g.getSymbolTable();
+        nullable = new IntBitSet();
+        g.addNullable(nullable);
+        firsts = g.firsts(nullable);
+        assertEquals(6, firsts.size());
+        assertEquals(PrefixSet.single(symbolTable.indexOf("b")), firsts.get(symbolTable.indexOf("Z")));
+        assertEquals(PrefixSet.single(symbolTable.indexOf("b")), firsts.get(symbolTable.indexOf("S")));
+        assertEquals(PrefixSet.single(symbolTable.indexOf("a")), firsts.get(symbolTable.indexOf("A")));
     }
 }
