3bca02081da4c45c6e5d101084c1b2229eff2b30
mlhartme <mlhartme@a73564bc-5afb-411d-915a-889d56b9fb7d>
2012-01-17 12:34:38 +0000

    tweaks

--- src/main/java/net/sf/beezle/mork/grammar/Concat.java
+++ src/main/java/net/sf/beezle/mork/grammar/Concat.java
@@ -2,13 +2,25 @@ package net.sf.beezle.mork.grammar;
 
 public class Concat {
     private final int k;
+    private final long firstFullValue;
     private final PrefixSet done;
     private PrefixSet todo;
 
     public Concat(int k) {
         this.k = k;
+        this.firstFullValue = exp(Prefix.BASE, k - 1);
         this.done = new PrefixSet();
         this.todo = PrefixSet.one();
+    }
+
+    private static long exp(int x, int y) {
+        long result;
+
+        result = 1;
+        for (int i = 1; i <= y; i++) {
+            result *= x;
+        }
+        return result;
     }
 
     /** true when done */
@@ -24,7 +36,7 @@ public class Concat {
             r = op.iterator();
             while (r.step()) {
                 tmp = Prefix.concat(l.data, r.data, k);
-                if (Prefix.size(tmp) == k) {
+                if (tmp >= firstFullValue) {
                     done.add(tmp);
                 } else {
                     next.add(tmp);
